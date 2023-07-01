6900eac5f375ba5bd32188ac9d5706266d319f65
Musili Brian <musilibrian07@gmail.com>
2023-07-01 06:03:15 +0300

    search functionality implementation

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -4,17 +4,58 @@ import PromptCard from "./PromptCard";
 
 const PromptCardList = ({ data, handleTagClick }) => (
   <div className="mt-16 prompt_layout">
-    {data && data.map((post) => (
-      <PromptCard key={post._id} post={post} handleTagClick={handleTagClick} />
-    ))}
+    {data &&
+      data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
   </div>
 );
 
 const Feed = () => {
+  // search state
   const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
+
+  //
   const [posts, setPosts] = useState([]);
 
-  const handleSearchChange = async (e) => {};
+  //filter prompts
+
+  const filterPrompts = (searchItem) => {
+    return posts.filter(
+      (item) =>
+        item.prompt.toLowerCase().includes(searchItem.toLowerCase()) ||
+        item.tag.toLowerCase().includes(searchItem.toLowerCase()) ||
+        item.creator.username.toLowerCase().includes(searchItem.toLowerCase())
+    );
+  };
+
+  const handleSearchChange = async (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  //implementing search by clicking tag name
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -22,7 +63,7 @@ const Feed = () => {
 
       const data = await response.json();
 
-      setPosts(data)
+      setPosts(data);
     };
 
     fetchPosts();
@@ -45,7 +86,11 @@ const Feed = () => {
 
       {/*  */}
 
-      <PromptCardList data={posts} handleTagClick={() => {}} />
+      {searchText && searchText ? (
+        <PromptCardList data={searchedResults} handleTagClick={() => {}} />
+      ) : (
+        <PromptCardList data={posts} handleTagClick={handleTagClick} />
+      )}
     </section>
   );
 };
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,13 +6,19 @@ import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession();
+
+  const pathName = usePathname();
+
+  const router = useRouter();
+
   const [copy, setCopy] = useState("");
 
   const handleCopy = () => {
-    setCopy(post.prompt)
-    navigator.clipboard.writeText(post.prompt)
-    setTimeout(() => setCopy(""), 3000)
-  }
+    setCopy(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopy(""), 3000);
+  };
 
   return (
     <div className="prompt_card">
@@ -58,8 +64,25 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         className="font-inter text-sm blue_gradient cursor-pointer"
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
-        {post.tag}
+        #{post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm green_gradient  cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer "
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
